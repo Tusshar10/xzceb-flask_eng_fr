b4a802d596ebb75b83279c43ce2b23be835299b7
Tusshar10 <131467401+Tusshar10@users.noreply.github.com>
2023-07-01 18:57:47 +0530

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,25 @@
+# pylint: skip-file
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-
+import machinetranslation
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    f=translator.englishtofrench(textToTranslate)
+    return f
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    e=translator.frenchtoenglish(textToTranslate)
+    return e
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
